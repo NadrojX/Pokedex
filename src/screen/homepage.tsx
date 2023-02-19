7247317d6dd0ad80bbad592b46bb5d04289c9483
GALATOLA Jordan <jordan.galatola@etu.univ-amu.fr>
2023-02-19 15:42:36 +0100

    Ajout de la barre de recherche et modification de la variable pour afficher tous les Pokémon sur la page d'accueil.

--- src/screen/homepage.tsx
+++ src/screen/homepage.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { PokemonHome } from "../component/PokemonHome";
 
 export const Homepage = () => {
+
+    let [pokemonSearchName, setPokemonSearchName] = useState('');
+    let navigate = useNavigate();
+
     return (
         <div>
             <h1 className="text-3xl font-bold flex justify-center mt-6 hover:underline">PokéDex</h1>
-            <form className="flex justify-center mt-6">
+            <form className="flex justify-center mt-6" onSubmit={(event2) => {navigate("/" + pokemonSearchName)}}>
                 <div>
                     <label className="block ml-7 mb-2 font-semibold" htmlFor="search">Pokémon Search</label>
-                    <input className="border rounded-2xl text-center" type="text" placeholder="Pokémon Name ?" name="search" id="search"/>
+                    <input onChange={(event) => setPokemonSearchName(event.target.value)} className="border rounded-2xl text-center" type="text" placeholder="Pokémon Name ?" name="search" id="search"/>
                 </div>
                <div className="ml-5 mt-7">
                     <input type='submit' className="block font-semibold px-2 py-1 rounded-xl bg-gray-700 hover:text-green-300"/>
